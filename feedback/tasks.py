e0b07186b1beee3f260abc894d81d272e3ede005
Sergey <ser.klionoff@yandex.ru>
2022-11-28 14:42:13 +0300

    Delete simulate expensive operation.

--- feedback/tasks.py
+++ feedback/tasks.py
@@ -7,7 +7,6 @@ from django.core.mail import send_mail
 @shared_task()
 def send_feedback_email_task(email_address, message):
     """Sends an email when the feedback form has been submitted."""
-    sleep(20)  # Simulate expensive operation that freezes Django
     send_mail(
         "Your Feedback",
         f"\t{message}\n\nThank you!",
